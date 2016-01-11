90f912f948018efc96b32f6ad7ee86ed28b34321
Jesse Eichar <Jesse.Eichar@swisscom.com>
2016-01-11 13:11:20 +0100

    material

--- app/project-browser.component.ts
+++ app/project-browser.component.ts
@@ -1,19 +1,12 @@
 import {Component, OnInit} from 'angular2/core';
 @Component({
-    selector: 'my-app',
+    selector: 'project-browser',
     template: `
-    <h1>{{title}}</h1>
-    <h2>My Heroes</h2>
-    <ul class="heroes">
-      <li *ngFor="#hero of heroes"
-        [class.selected]="hero === selectedHero"
-        (click)="onSelect(hero)">
-        <span class="badge">{{hero.id}}</span> {{hero.name}}
-      </li>
-    </ul>
-    <my-hero-detail [hero]="selectedHero"></my-hero-detail>
-  `})
-export class AppComponent implements OnInit {
+    <h2>Projects</h2>
+    <md-checkbox>Please check me</md-checkbox>
+  `
+})
+export class ProjectBrowserComponent implements OnInit {
     ngOnInit() {
     }
 }
