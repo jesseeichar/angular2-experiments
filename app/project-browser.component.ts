5412980522cd8d78486332d0d9ea18dde6fda49f
unknown <jeei@mima-ws-35.mimacom.local>
2016-01-11 11:11:29 +0100

    basic project structure

---
+++ app/project-browser.component.ts
@@ -0,0 +1,19 @@
+import {Component, OnInit} from 'angular2/core';
+@Component({
+    selector: 'my-app',
+    template: `
+    <h1>{{title}}</h1>
+    <h2>My Heroes</h2>
+    <ul class="heroes">
+      <li *ngFor="#hero of heroes"
+        [class.selected]="hero === selectedHero"
+        (click)="onSelect(hero)">
+        <span class="badge">{{hero.id}}</span> {{hero.name}}
+      </li>
+    </ul>
+    <my-hero-detail [hero]="selectedHero"></my-hero-detail>
+  `})
+export class AppComponent implements OnInit {
+    ngOnInit() {
+    }
+}
