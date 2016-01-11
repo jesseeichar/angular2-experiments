53b969683daa3af15d9ae69d47551dec6edd244c
Jesse Eichar <Jesse.Eichar@swisscom.com>
2016-01-11 18:07:36 +0100

    Project Explorer functioning

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,17 +4,19 @@ import {ProjectBrowserComponent} from './project-browser.component';
 @Component({
     selector: 'project-app',
     template: `
-      <h1>{{title}}</h1>
-
-    <!-- Container #2 -->
-    <div class="project-browser">
-        <project-browser></project-browser>
+    <nav>
+        <div class="nav-wrapper">
+        <a class="brand-logo">Project Manager</a>
+        </div>
+    </nav>
+    <div class="row">
+        <div class="col s4"><project-browser></project-browser></div>
+        <div class="col s8">This div is 8-columns wide</div>
     </div>
   `,
     directives: [ProjectBrowserComponent]
 })
 export class AppComponent implements OnInit {
-    public title = "Project Manager";
     ngOnInit() {
     }
 }
--- app/project-browser.component.ts
+++ app/project-browser.component.ts
@@ -1,12 +1,37 @@
 import {Component, OnInit} from 'angular2/core';
+import {ProjectItem} from './project-item'
+import {ProjectItemComponent} from './project-item.component'
 @Component({
     selector: 'project-browser',
     template: `
-    <h2>Projects</h2>
-    <md-checkbox>Please check me</md-checkbox>
-  `
+    <div class="card-panel">
+        <h5>Projects</h5>
+        <div class="collection">
+            <project-item *ngFor="#project of projects"
+                      [project]="project"
+                      [selectedProject]=selected
+                      (selected)="selected = $event"></project-item>
+        </div>
+    </div>
+  `,
+  directives: [ProjectItemComponent]
 })
 export class ProjectBrowserComponent implements OnInit {
+    public projects: ProjectItem[] = [
+        {name: "Project1", children: []},
+        {name: "Project2", children: [
+            {name: "Project21", children: []},
+            {name: "Project22", children: []},
+            {name: "Project23", children: [
+                {name: "Project231", children: []},
+                {name: "Project232", children: []},
+                {name: "Project232", children: []},
+            ]},
+        ]},
+        {name: "Project3", children: []},
+        {name: "Project4", children: []},
+    ]
+    public selected: ProjectItem;
     ngOnInit() {
     }
 }
